9b8d701cde863aaac13aba21fc24b937062ffe9b
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-06-11 01:40:36 +0530

    ui: Add hover text for the edit info link

--- main.mjs
+++ main.mjs
@@ -20,7 +20,7 @@ const Song = ({ song, isCurrent, playing, playPause, elem }) => {
   const description = eval("`" + songDescription + "`");
   const metadataLink = song.metadata_link
     ? html`<a class="song-info-link" href="${song.metadata_link}" target="_blank">
-        <span class="material-icons">rule</span>
+        <span title="Edit song info" class="material-icons">rule</span>
       </a>`
     : undefined;
   const playIcon = playing
