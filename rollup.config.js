9dbd869825a0858c2dbb97a984fad2a46ceee703
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-06-14 03:02:44 +0530

    ui: Configure rollup to be able to use jsx and react compat

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,22 @@
 import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import alias from "@rollup/plugin-alias";
 
 export default {
   input: "static/main.mjs",
-  plugins: [resolve()],
+  plugins: [
+    alias({
+      entries: [
+        { find: "react", replacement: "preact/compat" },
+        { find: "react-dom", replacement: "preact/compat" },
+      ],
+    }),
+    babel({
+      presets: ["@babel/preset-react"],
+      plugins: [["@babel/transform-react-jsx", { pragma: "h" }]],
+    }),
+    resolve(),
+  ],
   external: [
     "https://cdn.skypack.dev/pin/plyr@v3.6.8-UdIeSiyxM89uOBn4h5ie/mode=imports,min/optimized/plyr.js",
   ],
