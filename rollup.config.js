3437b39d6f6af6143b70851c4c3fdd8d1947436f
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-06-16 00:30:12 +0530

    ui: Add material-ui/{core,icons} as dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import alias from "@rollup/plugin-alias";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: "static/main.mjs",
@@ -15,7 +17,12 @@ export default {
       babelHelpers: "bundled",
       presets: ["@babel/preset-react"],
     }),
+    commonjs(),
     resolve(),
+    replace({
+      preventAssignment: true,
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
   ],
   external: [
     "https://cdn.skypack.dev/pin/plyr@v3.6.8-UdIeSiyxM89uOBn4h5ie/mode=imports,min/optimized/plyr.js",
