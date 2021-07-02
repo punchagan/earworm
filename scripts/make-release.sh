51a34da0334ae55adf8bef9a9e5b5890a774db97
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-07-03 00:47:52 +0530

    make-release: Print helpful message when no version supplied

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -2,8 +2,14 @@
 
 set -eu
 
-VERSION=$1
+VERSION=${1:-}
 RULES=("patch" "minor" "major" "prepatch" "preminor" "premajor" "prerelease")
+
+if [ -z $VERSION ];
+then
+    echo "Choose a version: ${RULES[@]}"
+    exit 1
+fi
 
 FOUND=""
 case "${RULES[@]}" in
